8bf892152968a8c0c52f00c059cfe2ea008bf178
Forcemagic <speedy@speedyblur.com>
2017-08-08 14:26:22 -0700

    Almost complete graph
    * 1 localization for lang HU is missing
    * Graph's y axis indicators are clipped

--- app/src/main/java/com/speedyblur/shared/Vars.java
+++ app/src/main/java/com/speedyblur/shared/Vars.java
@@ -1,8 +1,13 @@
 package com.speedyblur.shared;
 
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.HashMap;
+
 public class Vars {
     public static final String APIBASE = "https://www.speedyblur.com/kretaapi/v2.0";
     public static String AUTHTOKEN = "";
     public static String SQLCRYPT_PWD = "weeee";
     public static int VIEWPAGER_SCROLLDUR = 500;
+    public static HashMap<String, LineDataSet> averageGraphData = new HashMap<>();
 }
