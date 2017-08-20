c144168606c15a1b8d3b69fe64b7aafd1031d0cd
Matthew Coal <speedy@speedyblur.com>
2017-08-20 09:23:28 -0700

    Quickfix: Use onMeasure instead of onDraw
    Patched HeightExpandedListView to avoid the scrolling bug at date-ordered grades

--- app/src/main/java/com/speedyblur/models/HeightExpandedListView.java
+++ app/src/main/java/com/speedyblur/models/HeightExpandedListView.java
@@ -1,29 +1,27 @@
 package com.speedyblur.models;
 
 import android.content.Context;
-import android.graphics.Canvas;
 import android.util.AttributeSet;
-import android.view.ViewGroup;
 import android.widget.ListView;
 
 public class HeightExpandedListView extends ListView {
-
-    private ViewGroup.LayoutParams params;
-    private int oldCount = 0;
 
     public HeightExpandedListView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
+    public HeightExpandedListView(Context context) {
+        super(context);
+    }
+
+    public HeightExpandedListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
     @Override
-    protected void onDraw(Canvas canvas) {
-        if (getCount() != oldCount) {
-            int height = getChildAt(0).getHeight() + 1;
-            oldCount = getCount();
-            params = getLayoutParams();
-            params.height = getCount() * height;
-            setLayoutParams(params);
-        }
-        super.onDraw(canvas);
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
     }
 }
