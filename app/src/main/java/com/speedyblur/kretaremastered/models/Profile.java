3929d7d6e4952b4d05b2021f7fa7aaa13e77e93d
Matthew Coal <speedy@speedyblur.com>
2017-10-12 09:41:30 +0200

    Remove Parcelable implementation from Profile
    Fixes #38

--- app/src/main/java/com/speedyblur/kretaremastered/models/Profile.java
+++ app/src/main/java/com/speedyblur/kretaremastered/models/Profile.java
@@ -1,9 +1,6 @@
 package com.speedyblur.kretaremastered.models;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
-public class Profile implements Parcelable {
+public class Profile {
     private final String cardid;
     private final String passwd;
     private final String friendlyName;
@@ -14,40 +11,8 @@ public class Profile implements Parcelable {
         this.friendlyName = friendlyName;
     }
 
-    @SuppressWarnings("WeakerAccess")
-    protected Profile(Parcel in) {
-        cardid = in.readString();
-        passwd = in.readString();
-        friendlyName = in.readString();
-    }
-
-    public static final Creator<Profile> CREATOR = new Creator<Profile>() {
-        @Override
-        public Profile createFromParcel(Parcel in) {
-            return new Profile(in);
-        }
-
-        @Override
-        public Profile[] newArray(int size) {
-            return new Profile[size];
-        }
-    };
-
     public boolean hasFriendlyName() {
         return !this.friendlyName.equals("");
-    }
-
-    @Override
-    public int describeContents() {
-        // Method stub
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel parcel, int i) {
-        parcel.writeString(cardid);
-        parcel.writeString(passwd);
-        parcel.writeString(friendlyName);
     }
 
     // Getter methods
